051a818fc86b1eee9255fd785f6a55b3e2523a3a
Yatika Jena <j.yatika@iitg.ac.in>
2024-07-31 12:37:38 +0530

    did placeholder screens for not in mvp features

--- lib/screens/saved_notifications.dart
+++ lib/screens/saved_notifications.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
+import '../shared/my_colors.dart';
+import '../shared/my_styles.dart';
+
 class SavedNotifications extends StatelessWidget {
   const SavedNotifications({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: MyColors.getBackgroundGradient(),
+        ),
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: GradientText('saved notifications feature coming soon!!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                gradient: MyColors.getTextGradient()),
+          ),
+        ),
+      ),
+    );
+    ;
   }
 }
